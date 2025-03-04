79fa0a3ef17ddd043ee3ca85c66a3168a4d30ffb
manueeeeeeo <manu.engenios@gamil.com>
2025-03-04 19:40:28 +0100

    Implementaciones y librerias agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.clase.escuchoman"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.clase.escuchoman"
@@ -40,5 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.acrcloud:acrcloudsdk:1.0.20")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("org.jsoup:jsoup:1.15.3")
 }
